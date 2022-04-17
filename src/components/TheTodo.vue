84947096af813b91d86ac3009db99c292cca1b6e
Furkan <f.ceylan1@gmx.de>
2022-04-17 22:15:32 +0200

    reverted date input

--- src/components/TheTodo.vue
+++ src/components/TheTodo.vue
@@ -15,10 +15,9 @@
             <div class="add-todo">
                 <h3>Add New ToDo</h3>
                 <div class="add-todo-field">
-                    <input class="todo-input" type="text" v-model="newTodo" placeholder="What needs to be done?">
+                    <input type="text" v-model="newTodo" placeholder="What needs to be done?">
                     <button class="add-button" @click.prevent="addNewActive"><i class="fa-solid fa-plus"></i></button>
                 </div>
-                    <input class="date-input" type="date" v-model="newDate">
             </div>
 
             <!-- ACTIVE TODO -->
@@ -26,9 +25,8 @@
                 <h3>Active ToDo's: <span v-if="count > 0">{{ count }}</span></h3>
                 <h4 class="no-todos" v-if="activeTodo.length === 0">No todo's added yet</h4>
                 <div class="active-todo-content" v-else v-for="(listItem, idx) in activeTodo" :key="listItem">
-                    <p @click="toggleTodo(listItem)">
-                        <span :class="listItem.done ? 'todo-done' : ''">{{ listItem.todoItem }}</span> 
-                        <span class="todo-date" :class="listItem.done ? 'todo-done-date' : ''">{{ listItem.todoDate }}</span>
+                    <p :class="listItem.done ? 'todo-done' : ''" @click="toggleTodo(listItem)">
+                        {{ listItem.todoItem }}
                     </p>
                     <button type="button" class="delete-button remove" @click="removeActiveTodo(idx)">
                         <i class="fa-solid fa-xmark"></i>
@@ -65,7 +63,6 @@
       data() {
         return {
             newTodo: "",
-            newDate: "",
             activeTodo: [],
         };
       },
@@ -87,7 +84,7 @@
 
       computed: {
 
-        count() { return this.activeTodo.length; },
+        count() { return this.activeTodo.length; }
       },
 
       methods: {
@@ -96,16 +93,15 @@
 
             if (this.newTodo.trim() === "") return;
 
-            this.activeTodo.push({ "todoItem" : this.newTodo, "todoDate": this.newDate, "done": false });
+            this.activeTodo.push({ "todoItem" : this.newTodo, "done": false });
             this.newTodo = "";
-            this.newDate = "";
 
             localStorage.setItem("activeTodo", JSON.stringify(this.activeTodo))
         },
 
         removeActiveTodo(idx) { 
 
-            this.activeTodo.splice(idx, 1);
+            this.activeTodo.splice(idx, 1); 
 
             localStorage.setItem("activeTodo", JSON.stringify(this.activeTodo))
         },
@@ -115,6 +111,7 @@
             this.activeTodo = [];
 
             localStorage.setItem("activeTodo", JSON.stringify(this.activeTodo))
+
         },
 
         toggleTodo(listItem) {
@@ -122,7 +119,7 @@
             listItem.done = !listItem.done; 
 
             localStorage.setItem("activeTodo", JSON.stringify(this.activeTodo))
-        },
+        }
 
       },
 
